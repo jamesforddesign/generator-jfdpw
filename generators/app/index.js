dfbe7724abf3076badb13461b0c05b8554b2ebc3
David Beesley <dbeesley@jfd.co.uk>
2016-09-13 17:26:15 +0100

    Getting Database server details, Stalfos and ProcessWire (Master)

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,35 +1,65 @@
 'use strict';
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
-    scaffoldFolders: function () {
-        this.mkdir("website");
+
+    _private_createDatabase: function (answers) {
+        console.log(answers.dbhost);
+
+
+    },
+
+    prompting: function () {
+        return this.prompt([{
+            type    : 'input',
+            name    : 'dbhost',
+            message : 'Your Database Hostname',
+            default : 'localhost'
+        },
+        {
+            type    : 'input',
+            name    : 'dbname',
+            message : 'Your Database Name'
+        },
+        {
+            type    : 'input',
+            name    : 'dbuser',
+            message : 'Your Database Username'
+        },
+        {
+            type    : 'password',
+            name    : 'dbpass',
+            message : 'Your Database Password'
+        },
+        {
+            type    : 'input',
+            name    : 'dbport',
+            message : 'Your Database Port',
+            default : '3306'
+        }]).then( function (answers) {
+            this._private_createDatabase(answers);
+        }.bind(this));
     },
 
     getStalfos: function () {
         this.remote('hankchizljaw', 'stalfos', function(err, remote) {
-            remote.copy('.', '');
+            remote.bulkDirectory('.', '.');
         });
     },
 
     getProcessWire: function () {
         this.remote('ryancramerdesign', 'ProcessWire', function(err, remote) {
-            remote.copy('.', 'website');
+            remote.bulkDirectory('.', 'website');
         });
     },
 
-    createDatabase: function () {
-
+    getTemplates: function () {
+        this.remote('jamesforddesign', 'base-processwire', function(err, remote) {
+            remote.bulkDirectory('.', 'website');
+        });
     },
 
     installProcessWire: function () {
-
-    },
-
-    getTemplates: function () {
-
     }
 
 });
